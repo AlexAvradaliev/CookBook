1c30af95779825dd4e3375f232f02a8a602c899f
aleksandarAvr <avradaliev@gmail.com>
2022-07-12 22:43:02 +0300

    create aside menu

---
+++ src/components/Aside-menu/AsideMenu.js
@@ -0,0 +1,28 @@
+import styles from './AsideMenu.module.css';
+
+const AsideMenu = (props) => {
+
+    return (
+        <aside className={styles.profile__aside}>
+            <ul>
+
+                <p className={styles.profile__aside__text}>Activity</p>
+
+                <li>
+                    <a href="#"><p>Recipes</p></a>
+                </li>
+                <li>
+                    <a href="#"><p>Comments</p></a>
+                </li>
+                <li>
+                    <a href="#"><p>Create Recipe</p></a>
+                </li>
+                <li>
+                    <a href="#"><p className={styles.active}>Edit Profile</p></a>
+                </li>
+            </ul>
+        </aside>
+    );
+};
+
+export default AsideMenu;
