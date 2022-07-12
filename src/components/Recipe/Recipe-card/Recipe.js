e2842130bac878cb01fdcb97f76f8b617363b0d4
aleksandarAvr <avradaliev@gmail.com>
2022-07-12 20:37:34 +0300

    create recipe card

---
+++ src/components/Recipe/Recipe-card/Recipe.js
@@ -0,0 +1,24 @@
+import styles from './Recipe.module.css';
+
+const Recipe = () => {
+
+    return (
+        <article className={styles.recipe}>
+        <a href="#">
+            <img className={styles.recipe__image}
+                src="https://res.cloudinary.com/dmalpxwu4/image/upload/v1644681073/Recipes/recipes/bj5n2rrfqximbb28zbtj.jpg"
+                alt="Carrot and Pear Sauce" />
+            <p className={styles.recipe__name}>Carrot and Pear Sauce </p>
+            <div className={styles.recipe__owner}>
+                <img src="assets/images/user.webp"
+                    alt="alex" />
+                <p>Alex Alex</p>
+            </div>
+        </a>
+        <button className={`${styles.btn} ${styles.btn__info}`}>Update</button>
+        <button className={`${styles.btn} ${styles.btn__danger}`}>Delete</button>
+    </article>
+    );
+};
+
+export default Recipe;
