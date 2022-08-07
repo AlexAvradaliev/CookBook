6637eaa772266545ecf21ef390fb99b781025ab5
aleksandarAvr <avradaliev@gmail.com>
2022-08-07 14:13:34 +0300

    delete comments

--- src/components/Comments/Recipe-comments/RecipeCommentCard.js
+++ src/components/Comments/Recipe-comments/RecipeCommentCard.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import moment from 'moment';
 
 import { useAuthContext } from '../../../context/AuthContext';
 import { useCommentContext } from '../context/commentContext';
@@ -33,44 +34,57 @@ const RecipeCommentCard = () => {
         });
     };
 
+    const deleteHandler = (id) => {
+        commentService.remove(id, user.accessToken)
+        .then(() => {
+            const filtred = comments.filter(x => x._id != id);
+            addComments(filtred);
+        })
+    };
+
     return (
         <>
-        {comments.map(x => 
-        (
-        <section key={x._id} className={styles.comment__card__wrapper}>
-              <article className={styles.comment__card__image__wrapper}>
-                <div className={styles.comment__recipe__image}>
-                <img src={x._ownerId.photo} alt={x._ownerId.lastName} />
-                </div>
-                <div className={styles.comment__recipe__name}>
-                    <p>{x._ownerId.firstName}{" "}{x._ownerId.lastName}</p>
-                    <span className={styles.comment__recipe__createdAt}>
-                     {/* {moment(x.createdAt).fromNow()}{" "}  */}
-                        </span>
-                </div>
-            </article>
-            <article className={styles.comment__card__button__wrapper}>
-                <div>
-                    <p>{x.text}</p>
-                </div>
+            {comments.map(x =>
+            (
+                <section key={x._id} className={styles.comment__card__wrapper}>
+                    <article className={styles.comment__card__image__wrapper}>
+                        <div className={styles.comment__recipe__image}>
+                            <img src={x._ownerId.photo} alt={x._ownerId.lastName} />
+                        </div>
+                        <div className={styles.comment__recipe__name}>
+                            <p>{x._ownerId.firstName}{" "}{x._ownerId.lastName}</p>
+                            <span className={styles.comment__recipe__createdAt}>
+                                {moment(x.createdAt).fromNow()}{" "} 
+                            </span>
+                        </div>
+                    </article>
+                    <article className={styles.comment__card__button__wrapper}>
+                        <div>
+                            <p>{x.text}</p>
+                        </div>
 
-                {user?._id && user?._id === x._ownerId._id && (
-                <div>
-                <button 
-                onClick={updateHandler} 
-                className={`${styles.btn} ${styles.btn__info}`}
-                id={x._id}
-                >
-                    Update
-                    </button>
-                    <button className={`${styles.btn} ${styles.btn__danger}`}>Delete</button>
-                </div>
-                )}
+                        {user?._id && user?._id === x._ownerId._id && (
+                            <div>
+                                <button
+                                    onClick={updateHandler}
+                                    className={`${styles.btn} ${styles.btn__info}`}
+                                    id={x._id}
+                                >
+                                    Update
+                                </button>
+                                <button
+                                onClick={() => deleteHandler(x._id)}
+                                    className={`${styles.btn} ${styles.btn__danger}`}
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        )}
 
-            </article> 
-        </section>
-        )
-        )}
+                    </article>
+                </section>
+            )
+            )}
         </>
     );
 };
