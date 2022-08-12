0480f1339a92ba82e1bd791a8e7959016f6f2c19
aleksandarAvr <avradaliev@gmail.com>
2022-08-12 15:41:31 +0300

    add search

--- src/components/Headers/Home-search/HomeSearch.js
+++ src/components/Headers/Home-search/HomeSearch.js
@@ -1,16 +1,42 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './HomeSearch.module.css';
 
 const HomeSearch = () => {
 
-    return (
-        <form className={styles.header__search}>
-        <h2 className={styles.header__search__text}>Discover the world best recipes &amp; foods</h2>
-        <div>
-            <i className="fas fa-search"></i>
-            <input type="text" className={styles.search} placeholder="What do you want to cook?" />
-        </div>
-    </form>
-    );
-};
+    const navigate = useNavigate();
+    const [term, setTerm] = useState('');
+    const searchHandler = (e) => {
+        e.preventDefault()
 
-export default HomeSearch;
+        if (term.trim()) {
+            navigate(`/search/?name=${term}&page=1`)
+        } else {
+            navigate('/')
+        }
+
+    };
+    const onChangeSearch = (e) => setTerm(e.target.value)
+
+        return (
+            <form className={styles.header__search} onSubmit={searchHandler}>
+                <h2
+                    className={styles.header__search__text}>
+                    Discover the world best recipes &amp; foods
+                </h2>
+                <div>
+                    <i className="fas fa-search"></i>
+                    <input
+                        type="text"
+                        name="search"
+                        className={styles.search}
+                        placeholder="What do you want to cook?"
+                        value={term}
+                        onChange={onChangeSearch}
+                    />
+                </div>
+            </form>
+        );
+    };
+
+    export default HomeSearch;
